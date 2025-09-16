ec4b518480f251c1f8fa8ab1dd1dfd35ef6a8fc7
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2025-09-16 23:50:20 +0900

    Add initial project files for ebpf-based-tcpdump

--- ebpf-based-tcpdump/src/main.rs
+++ ebpf-based-tcpdump/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use aya::Ebpf;
+use aya::programs::{CgroupSkb, CgroupSkbAttachType, CgroupAttachMode};
+
 fn main() {
-    println!("Hello, world!");
+    // load the BPF code
+    let mut ebpf = Ebpf::load_file("ebpf.o")?;
+
+    // get the `ingress_filter` program compiled into `ebpf.o`.
+    let ingress: &mut CgroupSkb = ebpf.program_mut("ingress_filter")?.try_into()?;
+
+    // load the program into the kernel
+    ingress.load()?;
+
+    // attach the program to the root cgroup. `ingress_filter` will be called for all
+    // incoming packets.
+    let cgroup = File::open("/sys/fs/cgroup/unified")?;
+    ingress.attach(cgroup, CgroupSkbAttachType::Ingress, CgroupAttachMode::AllowOverride)?;
 }
